cf4c4e31614cca07c8c2473f69c41a27204ed36b
Juan Ortega <juanakin0108@gmail.com>
2018-09-20 20:47:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function whisper(string) {
+   
+   return string.toLowerCase();
+   
+}
+
+function shout(string) {
+  
+  return string.toUpperCase();
+  
+}
+
+function logShout(string) {
+  
+ console.log(string.toUpperCase());
+  
+}
+
+function logWhisper(string) {
+  
+ console.log(string.toLowerCase());
+  
+}
+
+function sayHiToGrandma(string) {
+  
+  if (string.toLowerCase() === string ) {
+  
+ return string = "I can't hear you!";
+ 
+ } else if (string.toUpperCase() === string); {
+ 
+ return string = "YES INDEED!";
+  
+}
+
+}
+
+
