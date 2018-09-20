46040c228cfae6f06ec359272dcfb2e6723fc9cd
Juan Ortega <juanakin0108@gmail.com>
2018-09-20 21:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ function sayHiToGrandma(string) {
   
  return string = "I can't hear you!";
  
- } else if (string.toUpperCase() === string); {
+ } else if (string.toUpperCase() === string) {
  
  return string = "YES INDEED!";
   
- } else if (string === "I love you, Grandma."); {
+ } else if (string === "I love you, Grandma.") {
   
   return string = "I love you too!";
 
